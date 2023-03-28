f9bc87f569f522d302f2e411e5391bbdc8f57c49
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-03-28 23:13:44 +0200

    add pages

---
+++ task1/script.js
@@ -0,0 +1,87 @@
+const myForm = document.querySelector("#myForm")
+const userWrap = document.querySelector("#userWrap")
+const singleData = document.querySelector("#singleData")
+
+const heads = ["name", "age", "email"]
+
+const readFromStorage = (key=`tasks`) => JSON.parse(localStorage.getItem(key)) || []
+const writeToStorage = (data, key=`tasks`) => localStorage.setItem(key, JSON.stringify(data))
+
+const userObjCreator = (myForm)=>{
+    const user = { id:Date.now() }
+    heads.forEach( h => user[h] = myForm.elements[h].value )
+    return user
+}
+
+const addUser = (user)=>{
+    const allUsers = readFromStorage("users")
+    allUsers.push(user)
+    writeToStorage(allUsers, "users")
+}
+
+function createMyOwnElement (ele, parent, txt=null, classes=null){
+    const myElement = document.createElement(ele)
+    parent.appendChild(myElement)
+    if(txt) myElement.textContent=txt
+    if(classes) myElement.classList=classes
+    return myElement
+}
+
+if(myForm){
+    myForm.addEventListener("submit", function(e){
+        e.preventDefault()
+        const user = userObjCreator(myForm)
+        addUser(user)
+        window.location = "index.html"
+    })  
+}
+const deleteMyElement = (allUsers, i) =>{
+    allUsers.splice(i,1)
+    writeToStorage(allUsers, "users")
+    drawData()
+}
+const drawData = () =>{
+    userWrap.innerHTML=""
+    const allUsers = readFromStorage("users")
+    allUsers.forEach((user, i)=>{
+        const tr = createMyOwnElement("tr", userWrap)
+        createMyOwnElement("td", tr, user.id)
+        createMyOwnElement("td", tr, user.name)
+        createMyOwnElement("td", tr, user.email)
+        createMyOwnElement("td", tr, user.age)
+        const td = createMyOwnElement("td", tr)
+
+        const delBtn = createMyOwnElement("button", td, "Delete","mx-2 btn btn-danger")
+        delBtn.addEventListener("click", (e)=> deleteMyElement(allUsers, i))
+        const showBtn = createMyOwnElement("button", td, "Show","mx-2 btn btn-primary")
+        showBtn.addEventListener("click", ()=>{
+            localStorage.setItem("index", i)
+            window.location="show.html"
+        })
+        const editBtn = createMyOwnElement("button", td, "Edit","mx-2 btn btn-warning")
+    })
+}
+
+if(userWrap){
+    drawData()
+}
+
+if(singleData){
+    const index = localStorage.getItem("index")
+  const allUsers= readFromStorage("users")
+  createMyOwnElement("p", singleData, allUsers[index].name)  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
