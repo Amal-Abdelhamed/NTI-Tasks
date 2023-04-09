194fc7cd3c67c96cb19ce067dca7ad5829f97606
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-04-09 02:45:27 +0200

    set mongo for all logic

--- task4-mongo/app/controller/tasksController.js
+++ task4-mongo/app/controller/tasksController.js
@@ -1,22 +1,23 @@
 const dbJson = require('../helper/dbJsonFile')
 const connectDb = require('../../models/dbConnect')
+const ObjectId = require("mongodb").ObjectId
 const fileName = "models/tasks.json"
 
 class taskController {
 
     // render home page 
-    static allTasks = async(req,res)=>{
-        try{
-            connectDb(async(db)=>{
+    static allTasks = async (req, res) => {
+        try {
+            connectDb(async (db) => {
                 const allTasks = await db.collection("tasks").find().toArray()
                 res.render("allTasks", {
-                    pageTitle:"All Tasks", 
+                    pageTitle: "All Tasks",
                     allTasks,
                     hasData: allTasks.length
                 })
             })
         }
-        catch(e){
+        catch (e) {
             res.send(e)
         }
     }
@@ -33,7 +34,7 @@ class taskController {
     static addLogic = async (req, res) => {
         try {
             connectDb(async (db) => {
-           const task = await db.collection("tasks").insertOne(req.body)
+                const task = await db.collection("tasks").insertOne(req.body)
 
                 res.redirect("/")
             })
@@ -42,60 +43,126 @@ class taskController {
             res.send(e.message)
         }
     }
+
     // delete task
-    static delSingleLogic = (req, res) => {
-        let allTasks = dbJson.readData(fileName)
-        const id = req.params.id
-        const i = allTasks.findIndex(t => t.id != id)
-        allTasks.splice(i, 1)
-        dbJson.writeData(fileName, allTasks)
-        res.redirect("/")
+
+    static delSingleLogic = async (req, res) => {
+        try {
+            connectDb(async (db) =>
+                await db.collection("tasks")
+                    .deleteOne({ _id: new ObjectId(req.params.id) })
+            )
+            res.redirect("/")
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
 
     // delete all tasks
-    static delAllLogic = (req, res) => {
-        let allTasks = dbJson.readData(fileName)
-        allTasks.length = 0
-        dbJson.writeData(fileName, allTasks)
-        res.redirect("/")
+    static delAllLogic = async (req, res) => {
+        try {
+            connectDb(async (db) => await db.collection("tasks").remove())
+            res.redirect("/")
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
+
+
     // edit task
-    static editBtn = (req, res) => {
-        const id = req.params.id
-        const allTasks = dbJson.readData(fileName)
-        const task = allTasks.find(t => t.id == id)
-        res.render("edit",
-            { pageTitle: "edit page", task }
-        )
+
+
+    static editBtn = async (req, res) => {
+        try {
+            connectDb(async (db) => {
+                const task = await db.collection("tasks").findOne({
+                    _id: new ObjectId(req.params.id)
+                })
+                res.render("edit", {
+                    pageTitle: "Single Task",
+                    task
+                })
+
+            })
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
-    static editLogic = (req, res) => {
-        const allTasks = dbJson.readData(fileName)
-        const id = req.params.id
-        const index = allTasks.findIndex(t => t.id == id)
-        allTasks[index] = { id, ...req.query }
-        dbJson.writeData(fileName, allTasks)
-        res.redirect("/")
+    static editLogic = async (req, res) => {
+        try {
+            connectDb(async (db) => {
+                const task = await db.collection("tasks").updateOne(
+                    { _id: new ObjectId(req.params.id) },
+                    { $set: { ...req.query } }
+                    );
+                    const allTasks = await db.collection("tasks").find().toArray()
+                    res.render("allTasks", {
+                        pageTitle: "All Tasks",
+                        allTasks,
+                        hasData: allTasks.length
+                    })
+                
+            })
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
     // show single task
-    static showBtn = (req, res) => {
-        const id = req.params.id
-        const allTasks = dbJson.readData(fileName)
-        const task = allTasks.find(t => t.id == id)
-        res.render("show",
-            { pageTitle: "show task details", task }
-        )
+    static showBtn = async (req, res) => {
+        try {
+            connectDb(async (db) => {
+                const task = await db.collection("tasks").findOne({
+                    _id: new ObjectId(req.params.id)
+                })
+                res.render("show", {
+                    pageTitle: "Show Single task",
+                    task
+                })
 
+            })
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
 
     // activate status
 
-    static active = (req, res) => {
-        const id = req.params.id
-        const allTasks = dbJson.readData(fileName)
-        const task = allTasks.find(t => t.id == id)
-        task.status = "true"
-        dbJson.writeData(fileName, allTasks)
-        res.redirect("/")
+    // static active = async (req, res) => {
+    //     try {
+    //         connectDb(async (db) => {
+
+    //             const id = req.params.id
+    //             const task = await db.collection("tasks").findAndModify({
+    //                 _id: new ObjectId(id),
+    //                 status: "true"
+    //             })
+    //             res.send("gg")
+    //         })
+    //     }
+    //     catch (e) {
+    //         res.send(e.message)
+    //     }
+    // }
+
+    static active = async (req, res) => {
+        try {
+            connectDb(async (db) => {
+                const task = await db.collection("tasks").updateOne(
+                    { _id: new ObjectId(req.params.id) },
+                    { $set: { ...req.query } }, {status:"true"}
+                );
+                res.send(task)
+
+            })
+        }
+        catch (e) {
+            res.send(e)
+        }
     }
 
     // search for task
@@ -104,19 +171,20 @@ class taskController {
         try {
             const searchData = req.query.search
             connectDb(async (db) => {
-           const task = await db.collection("tasks").find({$or:[{"title": { $regex: `/${searchData}/`,i }}, {"content": searchData}]}).toArray()
+                const tasks = await db.collection("tasks").find({ $or: [{ "title": { $regex: searchData, $options: "i" } }, { "content": { $regex: searchData, $options: "i" } }] }).toArray()
 
-           res.send(task)
+                res.render("allTasks", {
+                    pageTitle: "search Tasks",
+                    allTasks: tasks,
+                    hasData: tasks.length
+
+                })
             })
         }
         catch (e) {
             res.send(e.message)
         }
     }
-    
-    
-   
-
 
 
 
